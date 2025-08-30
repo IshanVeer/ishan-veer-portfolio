c3b3420fbcd11c4f4c9eeb2fabd931e5c06611da
Ishan Veer <ishanpveer@gmail.com>
2025-08-30 10:44:10 +0530

    changed text issues

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -50,8 +50,7 @@ const Hero = () => {
             </div>
 
             <p className="text-white-50 md:text-xl relative z-10 pointer-events-none">
-              Hi, I’m Adrian, a developer based in Croatia with a passion for
-              code.
+              Hi, I’m Ishan, a developer based in India with a passion for code.
             </p>
 
             <Button
